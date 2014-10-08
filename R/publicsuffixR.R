4f6f59e3890790cc5b034b5ae85d7b393ed023e0
Keith Twombley <ktwombley@gmail.com>
2014-10-08 16:35:22 -0500

    test harness together. it passes...some tests.

--- R/publicsuffixR.R
+++ R/publicsuffixR.R
@@ -3,7 +3,11 @@
 psl_URL <- "https://publicsuffix.org/list/effective_tld_names.dat"
 
 psl_raw_list <- function() {
- readLines(file(psl_URL, "r"))
+  con <- file(psl_URL, "r")
+  lll<-readLines(con)
+  close(con)
+  return(lll)
+  
 }
 
 
@@ -18,87 +22,145 @@ if ('memoise' %in% installed.packages()) {
 
 
 create_rules <- function(intext) {
-  rules <- vector()
-  # The Public Suffix List consists of a series of lines, separated by \n.
-  for (aline in intext){
-    print(aline)
-    # Each line is only read up to the first whitespace; entire lines can also be commented using //.
-    if(substr(aline,1,2)=="//") {
-      print("skipping comment!")
-      next
-    }
-
-    # Each line which is not entirely whitespace or begins with a comment contains a rule.
   
-    aline <- strsplit(aline, split="(\\s+)", perl=TRUE)[1]
-    print(sprintf("chop whitespace: %s", aline))
-    
-    print(sprintf("length=%d", nchar(aline)))
-    
-    #wtf is this a bug?
-    if(nchar(aline)==0 || aline=="character(0)") {
-      print("skipping blank!")
-      next
-    }
-    
-    # A rule may begin with a "!" (exclamation mark). If it does, it is labelled as a "exception rule" 
-    
-    if(substr(aline,1,1)=="!") {
-      print("an exception!")
-      #  and then treated as if the exclamation mark is not present.
-      aline <- substring(aline, 2)
-      attr(aline, "is.except") <- TRUE
-      print(aline)      
-    } else {
-      attr(aline, "is.except") <- FALSE
-    }
-    # A domain or rule can be split into a list of labels using the separator "." (dot). The separator 
-    # is not part of any of the labels.
+  rules <- lapply(intext, FUN=create_a_rule)
+  return(rules[!is.na(rules)])
+  
+}
 
-    #Note: these are stored reversed
-    aline <- splitDomain(aline)
-    print("split up:")
-    print(aline)
-    
-    rules <- c(rules, aline)
-    # A domain is said to match a rule if, when the domain and rule are both split, and one compares
-    # the labels from the rule to the labels from the domain, beginning at the right hand end, one finds
-    # that for every pair either they are identical, or that the label from the rule is "*" (star). The
-    # domain may legitimately have labels remaining at the end of this matching process.
+create_a_rule <- function(aline) {
+  # Each line is only read up to the first whitespace; entire lines can also be commented using //.
+  if(substr(aline,1,2)=="//") {
+    return(NA)
   }
-  return(rules)
   
+  # Each line which is not entirely whitespace or begins with a comment contains a rule.
+  
+  aline <- strsplit(aline, split="(\\s+)", perl=TRUE)[1]
+  
+  #wtf is this a bug?
+  if(nchar(aline)==0 || aline=="character(0)") {
+    return(NA)
+  }
+  
+  # A rule may begin with a "!" (exclamation mark). If it does, it is labelled as a "exception rule" 
+  
+  if(substr(aline,1,1)=="!") {
+    #  and then treated as if the exclamation mark is not present.
+    aline <- substring(aline, 2)
+    is.except <- TRUE
+  } else {
+    is.except <- FALSE
+  }
+  # A domain or rule can be split into a list of labels using the separator "." (dot). The separator 
+  # is not part of any of the labels.
+  
+  #Note: these are stored reversed
+  aline <- splitDomain(aline)
+  attr(aline, 'is.except')<-is.except
+  return(aline)
 }
 
-matchDomain <- function(domainname, rule) {
+#The domain and all rules must be canonicalized in the normal way for hostnames - lower-case, Punycode (RFC 3492).
+#TODO: Punycode
+splitDomain <- function(domainname) as.vector(lapply(strsplit(tolower(as.character(domainname)), ".", fixed=TRUE), FUN=rev))[[1]]
+
+joinDomain <- function(splitdomain) paste(rev(splitdomain), sep="", collapse=".")
+
+                                                                                                                  
+matchDomain <- function(splitdomainname, rules) {
+  # A domain is said to match a rule if, when the domain and rule are both split, and one compares
+  # the labels from the rule to the labels from the domain, beginning at the right hand end, one finds
+  # that for every pair either they are identical, or that the label from the rule is "*" (star). The
+  # domain may legitimately have labels remaining at the end of this matching process.
+
+  if(!is.list(rules)) {
+    stop("rules doesn't seem to contain any rules.")
+  }
+  if(is.na(splitdomainname)) return(list())
   
   
+  eachfunc <- function(rule) {
+    if(length(splitdomainname)<length(rule)) {
+      return(FALSE)
+    }        
+    for(pos in 1:length(rule)) {
+      if(rule[pos]!="*" && rule[pos]!=splitdomainname[pos]) {
+        return(FALSE)
+      } 
+    }
+    return(TRUE)
+  }
+  
+  ans <- rules[sapply(rules, FUN=eachfunc)]
+  return(ans)  
 }
 
-splitDomain <- function(domainname) lapply(strsplit(as.character(domainname), ".", fixed=TRUE), FUN=rev)
 
 publicSuffix <- function(domainname) {
   #Pseudocode from https://publicsuffix.org/list/
   # Match domain against all rules and take note of the matching ones.
   allrules <- create_rules(raw_list())
   
+  sdn <- splitDomain(domainname)
   
+  matches <- matchDomain(sdn, allrules)
+  
+  prevailing_rule <- NA
   
   # If no rules match, the prevailing rule is "*".
-  # If more than one rule matches, the prevailing rule is the one which is an exception rule.
-  # If there is no matching exception rule, the prevailing rule is the one with the most labels.
-  # If the prevailing rule is a exception rule, modify it by removing the leftmost label.
-  # The public suffix is the set of labels from the domain which directly match the labels of the prevailing rule (joined by dots).
-  # The registered or registrable domain is the public suffix plus one additional label.
+  if(length(matches)==0) {
+    prevailing_rule <- "*"
+  } else {
   
-  return(FALSE)
+    # If more than one rule matches, the prevailing rule is the one which is an exception rule.
+    matching_exceptions <- matches[sapply(matches, function(ar) {attr(ar, 'is.except')})]
+    if(length(matching_exceptions)==1) {
+      prevailing_rule <- matching_exceptions[[1]]
+  
+      # If the prevailing rule is a exception rule, modify it by removing the leftmost label.
+      length(prevailing_rule) <- length(prevailing_rule)-1
+  
+    } else if(length(matching_exceptions>1)) {
+      stop(paste("multiple matching exceptions", matching_exceptions, collapse=","))
+    } else {
+    
+      # If there is no matching exception rule, the prevailing rule is the one with the most labels.
+      max_len <- max(sapply(matches, length))
+      
+      longest_match <- matches[sapply(matches, function(m) {length(m)==max_len})]
+      if(length(longest_match)==1) {
+        prevailing_rule <- longest_match[[1]]    
+      } else if (length(longest_match>1)) {
+        stop(paste("multiple longest matches", longest_match, collapse=","))
+      }
+    }
+  }
+  pubsuf <- vector()
+  regable <- NA
+  
+  # The public suffix is the set of labels from the domain which directly match the labels of the prevailing rule (joined by dots).
+  for(pos in 1:length(prevailing_rule)) {
+    if(prevailing_rule[pos]=="*" || (prevailing_rule[pos]==sdn[pos])) {
+      pubsuf[pos] <- sdn[pos]
+      regable[pos] <- sdn[pos]
+    } else {
+      break
+    }
+  }
+  
+  # The registered or registrable domain is the public suffix plus one additional label.
+  regable <- pubsuf
+  regable[length(regable)+1] <- sdn[length(regable)+1]
+  
+  return(list('public_suffix'=joinDomain(pubsuf), 'registerable'=joinDomain(regable)))
+  
 }
 
 
 checkPublicSuffix <- function(domainname, suffix) {
   my_suffix <- publicSuffix(domainname)
-  if(my_suffix!=suffix) {
-    stop(sprintf("Mismatch! publicSuffix(\"%s\") -> \"%s\" does not match \"%s\"", domainname,my_suffix,suffix))
-  }
-  
+  regable <- my_suffix$registerable
+  return(regable==tolower(suffix))
 }
+
